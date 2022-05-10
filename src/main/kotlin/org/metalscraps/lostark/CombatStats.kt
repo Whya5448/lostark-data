631ee9fe6ea331612c433e5935578e1ba2e04c42
작은모코코 <Whya5448@gmail.com>
2022-05-11 08:25:41 +0900

    add CombatStats

---
+++ src/main/kotlin/org/metalscraps/lostark/CombatStats.kt
@@ -0,0 +1,10 @@
+package org.metalscraps.lostark
+
+enum class CombatStats(val id: Int, val friendlyName: String) {
+    CRIT(15, "치명"),
+    SPECIALIZATION(16, "특화"),
+    SWIFTNESS(18, "신속"),
+    DOMINATION(17, "제압"),
+    ENDURANCE(19, "인내"),
+    EXPERTISE(20, "숙련");
+}
